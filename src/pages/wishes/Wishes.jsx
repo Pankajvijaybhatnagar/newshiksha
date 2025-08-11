50d486d4344c7fda1c4472e0dfcdfa010e30e6e4
Pankajvijaybhatnagar <pankajbhatnagar98@gmail.com>
2025-08-11 17:55:32 +0530

    updatd code1

--- src/pages/wishes/Wishes.jsx
+++ src/pages/wishes/Wishes.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Layout } from "../../layouts/Layout";
-import { Wish } from "../../components/wishes/wish";
+import { Wish } from "../../components/wishes/Wish";
 // import wishesImg from "../../assets/img/breadcrumb/wishes.jpg";
 
 export const Wishes = () => {
