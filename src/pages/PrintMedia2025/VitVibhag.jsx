67f3c3eed7ec2430bb4ec7ce78f226fb22e69a70
ankitjnvynr <ankitbkana@outlook.com>
2025-08-20 12:15:21 +0530

    updarte images

---
+++ src/pages/PrintMedia2025/VitVibhag.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Layout } from "../../layouts/Layout";
+import { Vibhag2 } from "../../components/about/Vibhag2";
+
+export const VitVibhag = () => {
+  return (
+    <Layout
+      breadcrumbTitle={"Vitt Vibhag"}
+    >
+      <Vibhag2 />
+    </Layout>
+  );
+};
